5a520a7b0096b34ddd34e7a9e571bbafd4b81f88
Myphz <danielkeq@gmail.com>
2023-09-17 19:47:18 +0200

    chore: remove outlines

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   import { Card } from "$lib/components";
 </script>
 
-<div class="flex items-center justify-between">
+<div class="mb-9 flex items-center justify-between">
   <h1 class="font-headers text-h1">My Trips</h1>
   <button>
     <img src="/plus.svg" alt="plus" class="aspect-square h-6" />

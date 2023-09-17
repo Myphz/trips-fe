73c521a3a9af3387073093634402e5f90ab7502e
Myphz <danielkeq@gmail.com>
2023-09-17 19:54:12 +0200

    chore: remove user-select for buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
   import { Card } from "$lib/components";
+  import { Plus } from "svelte-heros";
 </script>
 
 <div class="mb-9 flex items-center justify-between">
   <h1 class="font-headers text-h1">My Trips</h1>
-  <button>
-    <img src="/plus.svg" alt="plus" class="aspect-square h-6" />
+  <button class="text-primary">
+    <Plus size="2rem" />
   </button>
 </div>
 <Card />

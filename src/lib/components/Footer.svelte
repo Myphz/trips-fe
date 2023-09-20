b489c29b2fe00127b3be0531187b2346b7498200
Myphz <danielkeq@gmail.com>
2023-09-20 18:42:09 +0200

    feat: disable footer if keyboard is open

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -4,6 +4,7 @@
   import Train from "$lib/assets/icons/train.svg?raw";
   import Trip from "$lib/assets/icons/trip.svg?raw";
   import { page } from "$app/stores";
+  import { keyboardOpen } from "$lib/stores/ui";
 
   const TABS = [
     { icon: Home, path: "/app" },
@@ -13,10 +14,12 @@
   ];
 </script>
 
-<nav class="fixed bottom-0 flex h-16 w-full items-center justify-between bg-primary px-9">
-  {#each TABS as { icon, path }}
-    <a class={$page.route.id === path ? "text-white" : "text-tab"} href={path}>
-      {@html icon}
-    </a>
-  {/each}
-</nav>
+{#if !$keyboardOpen}
+  <nav class="fixed bottom-0 flex h-16 w-full items-center justify-between bg-primary px-9">
+    {#each TABS as { icon, path }}
+      <a class={$page.route.id === path ? "text-white" : "text-tab"} href={path}>
+        {@html icon}
+      </a>
+    {/each}
+  </nav>
+{/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,9 +5,19 @@
   import { Navbar, Header, Footer } from "$lib/components";
   import { onMount } from "svelte";
   import { SvelteToast } from "@zerodevx/svelte-toast";
+  import { keyboardOpen } from "$lib/stores/ui";
 
   onMount(() => {
     import("../config");
+    if (!("visualViewport" in window && window.visualViewport)) return;
+
+    const VIEWPORT_VS_CLIENT_HEIGHT_RATIO = 0.75;
+
+    window.visualViewport.addEventListener("resize", function (event: any) {
+      const isOpen =
+        (event.target.height * event.target.scale) / window.screen.height < VIEWPORT_VS_CLIENT_HEIGHT_RATIO;
+      keyboardOpen.set(isOpen);
+    });
   });
 </script>
 

e554933dc53799e594c0da7b8bb973bac1cdf0a7
Myphz <danielkeq@gmail.com>
2023-09-30 13:18:05 +0200

    fix: build error

--- src/routes/app/trip/+page.svelte
+++ src/routes/app/trip/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+  import { page } from "$app/stores";
   import Cards from "$lib/components/cards/Cards.svelte";
   import { card } from "$lib/stores/api/select";
   import { setPageTitle } from "$lib/stores/route";
   import { getName } from "$utils/format";
 
-  $: window.location.pathname === "/app/trip" && setPageTitle(getName($card));
+  $: $page.route.id === "/app/trip" && setPageTitle(getName($card));
 </script>
 
 <Cards />

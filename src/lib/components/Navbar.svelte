ff6c3c8d9729764a16b8a1e3aa5396416d0bc1f1
Myphz <danielkeq@gmail.com>
2023-09-30 22:39:48 +0200

    feat: add avatar icon

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
   import Logo from "$lib/assets/brand/logo-outline.svg?raw";
+  import { UserCircle } from "svelte-heros";
 </script>
 
-<header class="sticky top-0 z-50 mb-12 flex h-16 items-center justify-between bg-gradient px-4 text-white">
+<header
+  class="sticky top-0 z-50 mb-12 flex h-16 items-center justify-between bg-gradient px-4 text-white"
+>
   <div class="flex aspect-square w-16 items-center justify-center">
     {@html Logo}
   </div>
 
-  <button>avatar</button>
+  <a href="/"><UserCircle size="2rem" /></a>
 </header>

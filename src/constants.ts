220664c10bfa9ad1eb964dd70b8e7f32c51d91ee
Myphz <danielkeq@gmail.com>
2023-10-02 23:09:56 +0200

    chore: update server url

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,4 @@ export const SUPABASE_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9zYm5udHFkd2NhbWhxYnBkend6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQxMTUzNjIsImV4cCI6MjAwOTY5MTM2Mn0.xGSk-6MSg_P7paUJIfZ_fAsK9hUikOj6iwOaQzRJaFg";
 export const SUPABASE_URL = "https://osbnntqdwcamhqbpdzwz.supabase.co";
 
-export const SERVER_URL = "https://tripssss.fly.dev";
+export const SERVER_URL = "https://tripss.fly.dev";

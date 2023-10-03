9eac7e7b77a4d0f2f395d3957bad02071452e48a
Myphz <danielkeq@gmail.com>
2023-10-03 13:58:02 +0200

    feat: add lodging card

--- src/lib/components/cards/Lodging.svelte
+++ src/lib/components/cards/Lodging.svelte
@@ -1,38 +1,19 @@
 <script lang="ts">
-  import type { EntityCommon, GetRowType } from "$lib/types/api";
+  import type { GetRowType } from "$lib/types/api";
   import { dateToCard } from "$utils/format";
-  import { Stars, Redirect } from "..";
+  import CardBase from "./CardBase.svelte";
 
-  export let data: EntityCommon & GetRowType<"lodging">;
-  const { id, tripId, rating, parent } = data;
+  export let data: GetRowType<"lodging">;
 </script>
 
-<Redirect href="/app/trip" params={{ entityId: id, tripId: tripId, parent }}>
-  <article class="relative flex h-52 w-full rounded-xl text-white">
-    <img
-      class="darker-image absolute -z-10 h-full w-full rounded-xl object-cover"
-      src="https://www.state.gov/wp-content/uploads/2019/04/Japan-2107x1406.jpg"
-      alt="test"
-    />
-
-    <div class="flex flex-1 flex-col pb-4 pl-2 pr-4 pt-2">
-      <!-- <div class="flex flex-1 flex-col">
-        <header class="text-h3">{name}</header>
-        {#if date}
-          <div class="text-xs">{dateToCard(date)}</div>
-        {/if}
+<CardBase {data}>
+  <svelte:fragment slot="header">
+    {#if data.start && data.end}
+      <div class="flex gap-1 text-xs">
+        <div class="text-xs">{dateToCard(data.start)}</div>
+        <span>-</span>
+        <div class="text-xs">{dateToCard(data.end)}</div>
       </div>
-      <div class="flex items-center justify-between">
-        <Stars number={rating} />
-      </div>
-    </div> -->
-      todo
-    </div>
-  </article>
-
-  <style>
-    .darker-image {
-      filter: brightness(50%);
-    }
-  </style>
-</Redirect>
+    {/if}
+  </svelte:fragment>
+</CardBase>

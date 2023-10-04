fa412890d9d619207a98ee0643b973157fe84e53
Myphz <danielkeq@gmail.com>
2023-10-04 11:13:18 +0200

    feat: new empty state

--- src/lib/components/cards/Empty.svelte
+++ src/lib/components/cards/Empty.svelte
@@ -1,9 +1,19 @@
+<script lang="ts">
+  import { filter } from "$lib/stores/api/select";
+</script>
+
 <article class="mt-10 flex w-full flex-col items-center justify-center text-gray">
   <img src="/empty.png" alt="Empty State" />
-  <header class="text-h3">Adventure Awaits!</header>
-  <div>Start planning now</div>
-  <div>
-    Tap <span class="text-primary">+</span>
-    to begin
-  </div>
+  {#if !$filter}
+    <header class="text-h3">Adventure Awaits!</header>
+    <div>Start planning now</div>
+    <div>
+      Tap <span class="text-primary">+</span>
+      to begin
+    </div>
+  {:else}
+    <header class="text-h3">Adventure Awaits!</header>
+    <div>No {$filter}s found</div>
+    <div>for the current trip.</div>
+  {/if}
 </article>

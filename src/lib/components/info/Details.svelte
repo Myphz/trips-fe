d671423d472c054af0d77aad32e58b27258ebdea
Myphz <danielkeq@gmail.com>
2023-10-09 12:29:32 +0200

    feat: show Details only if it has any entries

--- src/lib/components/info/Details.svelte
+++ src/lib/components/info/Details.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
   export let data: Record<string, string | number>;
   export let header: string;
+
+  const entries = Object.entries(data);
 </script>
 
-<section>
-  <header class="text-h3 capitalize">{header}</header>
-  <div class="mt-4 flex flex-col gap-1">
-    {#each Object.entries(data) as [key, value]}
-      <div class="flex justify-between">
-        <span class="capitalize">{key}</span>
-        <span class="capitalize">{value}</span>
-      </div>
-    {/each}
-  </div>
-</section>
+{#if entries.length}
+  <section>
+    <header class="text-h3 capitalize">{header}</header>
+    <div class="mt-4 flex flex-col gap-1">
+      {#each entries as [key, value]}
+        <div class="flex justify-between">
+          <span class="capitalize">{key}</span>
+          <span class="capitalize">{value}</span>
+        </div>
+      {/each}
+    </div>
+  </section>
+{/if}
